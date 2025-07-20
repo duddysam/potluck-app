2178173d77e808fa21c316ebfc73b83d20bdbf5d
duddysam <duddysam91@gmail.com>
2025-07-20 15:51:08 -0400

    Update events.py

--- routers/events.py
+++ routers/events.py
@@ -1,13 +1,27 @@
 from fastapi import APIRouter
 from models.event import EventCreate, CommentCreate
+from database.memory import events_db, comments_db, get_next_comment_id,  get_next_event_id
 
 router = APIRouter(prefix="/events", tags=["users"])
 
 @router.post("/create")
 def create_event(event: EventCreate):
+    event_id = get_next_event_id()
+    event_dict = event.model_dump()
+    event_dict['id'] = event_id
+    events_db['id'] = event_dict
+    return event_dict
 
-    return{"message": "Event created", "event_name": event.name, "event_date": event.date}
+@router.post("/{event_id}/create-comment/")
+def event_create_comment(event_id: int, comment: CommentCreate):
 
-@router.post("/create-comment/")
-def event_create_comment(comment: CommentCreate):
-    return {"message": "Comment created", "comment_text": comment.text, "created_by": comment.posted_by}
+    if event_id not in events_db:
+        return {"message": "no event was found."}
+
+    else:
+        comment_id = get_next_comment_id()
+        comment_dict = comment.model_dump()
+        comment_dict['event_id'] = event_id
+        comment_dict['id'] = comment_id
+        comments_db['id'] = comment_dict
+        return comment_dict
